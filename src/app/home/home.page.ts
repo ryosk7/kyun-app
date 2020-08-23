d66327c91caf8a11c9b4f18c05ca1afc7f48c500
ryosk7 <uchiryo7@gmail.com>
2020-08-23 23:13:14 +0900

    feat(kyun): きゅんきゅん機能を追加能

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,14 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  isPushed: boolean = false;
+  count: number = 0;
+
   constructor() {}
 
+  setCountUp(): number {
+    this.isPushed = true;
+    this.count = this.count + 1;
+    return this.count;
+  }
 }
